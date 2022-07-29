0592480c341bf6b124d35ed4fbe38564ad697514
badaruddinl <badarlut@gmail.com>
2022-07-29 20:02:37 +0700

    Bamboo 2

---
+++ bamboo2.py
@@ -0,0 +1,15 @@
+bamboos = [5,3,2,7,1]
+cuts = 5
+print("bamboos :", bamboos)
+print("cuts:"   ,cuts-1)
+for w in range(cuts):
+  if w == 0:
+    print("initials:")
+  else:
+    print("cycle cuts: "+str(w))
+  for x in range(len(bamboos)):
+    temp = "|"
+    for y in range(bamboos[x]):
+      temp = temp+"-"
+    print(temp)
+    bamboos[x] = bamboos[x]-1
