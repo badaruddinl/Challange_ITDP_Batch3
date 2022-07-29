267496eac5bc96926466994d61e637f713362b81
badaruddinl <badarlut@gmail.com>
2022-07-29 19:47:36 +0700

    Pola loop

--- bamboo1.py
+++ bamboo1.py
@@ -1,3 +1,9 @@
 bambu = [3,2,2] 
 cuts = 3
 print("cuts:"   ,cuts-1)
+print("Cycle : "+str(w))
+  for x in range(len(bambu)):
+    temp = "|"
+    for y in range(bambu[x]):
+      temp = temp+"-"
+    print(temp)
